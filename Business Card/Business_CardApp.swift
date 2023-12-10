4f361ba10959a32e63cdea05c45cfdd1cac0b926
Juan Ticante Vicente <juan.ticante@justo.mx>
2023-12-10 15:19:36 -0600

    Initial Commit

---
+++ Business Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business Card
+//
+//  Created by Juan Ticante on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
